2ef5477623448511027fd27c9d76da15900363f3
PunkHazard125 <mosabbir.ahmed125@gmail.com>
2026-04-09 22:06:38 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+	return (
+		<div className="bg-cod-gray-900 h-screen flex flex-col items-center justify-center">
+			<p className="font-ubuntu text-5xl font-bold text-pearl-bush-200">
+				Hello!
+			</p>
+		</div>
+	);
+}
+
+export default App;
