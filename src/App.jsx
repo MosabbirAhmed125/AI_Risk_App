e94730fb89b118461baec2f552678a97082aa3ff
PunkHazard125 <mosabbir.ahmed125@gmail.com>
2026-04-12 22:46:43 +0600

    Updated background colours

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,52 @@ function App() {
 
 	return (
 		<div>
-			<Toaster position="top-center" />
+			<Toaster
+				position="top-center"
+				toastOptions={{
+					className: "font-ubuntu font-bold",
+					style: {
+						background: "#e5e6e5",
+						color: "#1a1a1a",
+						border: "none",
+					},
+
+					success: {
+						iconTheme: {
+							primary: "#275ca2",
+							secondary: "#e5e6e5",
+						},
+						style: {
+							boxShadow: `
+								0 0 0 2px rgba(39,92,162,0.15),
+								0 6px 18px rgba(39,92,162,0.35),
+								0 2px 6px rgba(0,0,0,0.2)
+							`,
+						},
+					},
+
+					error: {
+						iconTheme: {
+							primary: "#e53945",
+							secondary: "#e5e6e5",
+						},
+						style: {
+							boxShadow: `
+								0 0 0 2px rgba(229,57,69,0.15),
+								0 6px 18px rgba(229,57,69,0.35),
+								0 2px 6px rgba(0,0,0,0.2)
+							`,
+						},
+					},
+
+					loading: {
+						iconTheme: {
+							primary: "#1a1a1a",
+							secondary: "#e5e6e5",
+						},
+					},
+				}}
+			/>
 
 			<Routes location={location} key={location.pathname}>
 				<Route
